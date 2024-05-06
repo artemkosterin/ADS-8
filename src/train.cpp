8edbdf2621d817d91ea2ea91d78aa87cd2703753
artemkosterin <160392965+artemkosterin@users.noreply.github.com>
2024-05-06 18:54:02 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,7 +1,7 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
 
-Train::Train() : countOp(0), first(nullptr) {}
+Train::Train() : countOp(0), first(nullptr), current(nullptr) {}
 
 void Train::addCage(bool light) {
     Cage* newCage = new Cage{light, nullptr, nullptr};
@@ -9,12 +9,16 @@ void Train::addCage(bool light) {
         first = newCage;
         first->next = first;
         first->prev = first;
+        current = first;
     } else {
         Cage* lastCage = first->prev;
         lastCage->next = newCage;
         newCage->prev = lastCage;
         newCage->next = first;
         first->prev = newCage;
+    }
+    if (current != nullptr) {
+        current = current->next;
         countOp++;
     }
 }
